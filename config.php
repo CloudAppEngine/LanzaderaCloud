9b426a265e37f2b52a4847b8c575bc3c4be35c00
jdelgadd <jdelgadd@everis.com>
2016-01-23 21:13:01 +0100

    PHP Datastore

---
+++ config.php
@@ -0,0 +1,6 @@
+	$google_api_config = [
+	  'application-id' => 'lanzaderacloud',
+	  'service-account-name' => 'jdelgadd@lanzaderacloud.iam.gserviceaccount.com',
+	  'private-key' => file_get_contents('LanzaderaCloudEngineApp-880e670339bf.p12'),
+	  'dataset-id' => 'lanzaderacloud'
+	];
